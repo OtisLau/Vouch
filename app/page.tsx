462f1e186b862129e206727fd3bd8c12fffee130
YanicB <yanic.ballez@gmail.com>
2025-11-08 21:24:59 -0500

    add navbar compoment

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/hero"
 import HowItWorks from "../components/howItWorks"
 import Footer from '../components/footer'
+import Navbar from '../components/navbar'
 export default function HomePage() {
     return (
         <main className="min-h-screen">
+            <Navbar />
             <Hero />
             <HowItWorks />
             <Footer />
